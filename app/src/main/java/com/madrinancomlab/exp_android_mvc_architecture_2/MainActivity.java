f2b39bfbee5c488893ae19bd1c60a657f3652b53
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2022-06-04 04:19:41 +0700

    MainActivity was connected to other classes

--- app/src/main/java/com/madrinancomlab/exp_android_mvc_architecture_2/MainActivity.java
+++ app/src/main/java/com/madrinancomlab/exp_android_mvc_architecture_2/MainActivity.java
@@ -6,20 +6,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.madrinancomlab.exp_android_mvc_architecture_2.Controller.InterfaceLoginController;
+import com.madrinancomlab.exp_android_mvc_architecture_2.Controller.LoginController;
+import com.madrinancomlab.exp_android_mvc_architecture_2.View.InterfaceLoginView;
 
 import java.util.Observable;
 import java.util.Observer;
 
-public class MainActivity extends AppCompatActivity implements Observer, View.OnClickListener
+public class MainActivity extends AppCompatActivity implements Observer, View.OnClickListener, InterfaceLoginView
 {
     EditText txtEmail, txtPassword;
     Button btnLogin;
+
+    InterfaceLoginController loginController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginController = new LoginController(this);
 
         txtEmail = findViewById(R.id.txtEmail);
         txtPassword = findViewById(R.id.txtPassword);
@@ -34,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements Observer, View.On
         switch (view.getId())
         {
             case R.id.btnLogin:
+                loginController.onLogin(txtEmail.getText().toString(), txtPassword.getText().toString());
                 break;
         }
     }
@@ -42,5 +52,19 @@ public class MainActivity extends AppCompatActivity implements Observer, View.On
     public void update(Observable observable, Object o)
     {
 
+    }
+
+    // THIS EVENT IS FROM InterfaceLoginView
+    @Override
+    public void onLoginSuccess(String message)
+    {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    // THIS EVENT IS FROM InterfaceLoginView
+    @Override
+    public void onLoginError(String message)
+    {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
